8dcc81d95e592907cf8e66c4e93762c104939324
anudeepmuppalla1729 <anudeepmuppalla@gmail.com>
2025-03-21 00:00:30 +0530

    Added profile pic upload feature for both agents and users.

--- server/controllers/setupControllers.js
+++ server/controllers/setupControllers.js
@@ -4,10 +4,10 @@ import Agent from "../models/agents.js";
 export const setupUser = async(req , res) =>{
     try{
         const {name , phone , dno , street, city, pincode} = req.body;
-        console.log(req.user)
+        if (!req.file) {
+            return res.status(400).json({ error: "No profile picture uploaded" });
+        }
         const userId = req.user.userId;
-        console.log(req.body);
-        console.log(userId);
         const user = await User.findById(userId);
         if(!user){
             return res.status(404).json({message: "User not found"});
@@ -15,6 +15,7 @@ export const setupUser = async(req , res) =>{
         user.name = name;
         user.phone = phone;
         user.address = {dno, street, city, pincode};
+        user.profilePicture = req.file.path;
         await user.save();
         res.status(201).json({message: "User updated successfully"});
     }catch(error){
@@ -26,6 +27,9 @@ export const setupAgent = async(req, res) =>{
     try{
         const {name, phone, dno, street, city, pincode , panCard , adhaarNumber} = req.body;
         const agentId = req.agent.id;
+        if(!req.file){
+            return res.status(400).json({error: "No profile picture uploaded"});
+        }
         const agent = await Agent.findById(agentId);
         if(!agent){
             return res.status(404).json({message: "Agent not found"});
@@ -35,6 +39,7 @@ export const setupAgent = async(req, res) =>{
         agent.address = {dno, street, city, pincode};
         agent.panCard = panCard;
         agent.aadharNumber = adhaarNumber;
+        agent.profilePicture = req.file.path;
         await agent.save();
         res.status(201).json({message: "Agent updated successfully"});
     }catch(error){
